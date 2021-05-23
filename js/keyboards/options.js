003c0b0a0450f78240ca65b6741c3516e0a8e9ba
Misha <libchenkomikhail@gmail.com>
2021-05-23 18:53:07 +0300

    Some refactor

--- js/keyboards/options.js
+++ js/keyboards/options.js
@@ -12,10 +12,7 @@ module.exports = {
             inline_keyboard: [
                 [{text: '1я лаба (фигуры)', callback_data: '1'}, {text: '2я лаба (полином)', callback_data: '2'}],
                 [{text: '3я лаба (парсер)', callback_data: '3'}, {text: '4я лаба (шаблоны)', callback_data: '4'}],
-                [{text: '5я лаба (кольцевой буффер)', callback_data: '5'}, {
-                    text: '6я лаба (кубик)',
-                    callback_data: '6'
-                }]
+                [{text: '5я лаба (кольцевой буффер)', callback_data: '5'}, {text: '6я лаба (кубик)', callback_data: '6'}]
             ]
         })
     },
